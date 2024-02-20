de228be9bed2dc577cb6196c3835099dbd51a75b
Samuel Alvarado <Hector.AD@AlliedGlobal.com>
2024-02-20 09:16:15 -0600

    agregando MongoDB para manejo de base de datos no transaccional agregando la estructura de manejo de la data

---
+++ src/main/java/com/rdmfinal/TablesReservation_Final/application/service/MongoDB/TableBenchServiceMongoDB.java
@@ -0,0 +1,44 @@
+package com.rdmfinal.TablesReservation_Final.application.service.MongoDB;
+
+import com.rdmfinal.TablesReservation_Final.application.exception.DemoSecurityException;
+import com.rdmfinal.TablesReservation_Final.application.lasting.EMessage;
+import com.rdmfinal.TablesReservation_Final.domain.dto.TableBenchDTO;
+import com.rdmfinal.TablesReservation_Final.domain.entity.PostGreSQL.TableBench;
+import com.rdmfinal.TablesReservation_Final.domain.repository.PostGreSQL.TableBenchRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public record TableBenchService(TableBenchRepository tableBenchRepository) {
+    public void createMesa(TableBenchDTO tableBenchDTO){
+        TableBench tableBench = TableBench.builder()
+                .noPersonas(tableBenchDTO.noPersonas())
+                .ubicacion(tableBenchDTO.ubicacion())
+                .reservada(tableBenchDTO.reservada())
+                .build();
+
+        System.out.println("Se ha ingresado TableBench correctamente: "+ tableBench);
+        // llamar a la interface del repository save
+        tableBenchRepository.save(tableBench);
+    }
+
+    public TableBenchDTO findMesaById(Long idMesa) throws DemoSecurityException {
+        TableBench tableBench = tableBenchRepository.findById(idMesa)
+                .orElseThrow(
+                        () -> new DemoSecurityException(EMessage.DATA_NOT_FOUND)
+                );
+        return new TableBenchDTO(
+                tableBench.getIdMesa(),
+                tableBench.getNoPersonas(),
+                tableBench.getUbicacion(),
+                tableBench.isReservada()
+        );
+    }
+
+    public void deleteMesaByID(Long idMesa) throws DemoSecurityException {
+        TableBench tableBench = tableBenchRepository.findById(idMesa)
+                .orElseThrow(
+                        () -> new DemoSecurityException(EMessage.DATA_NOT_FOUND)
+                );
+        tableBenchRepository.deleteById(idMesa);
+    }
+}
