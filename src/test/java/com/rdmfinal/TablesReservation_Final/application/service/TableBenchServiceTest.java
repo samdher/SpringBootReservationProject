ce37b1290351f493f2a34feceef882cccae1ab60
Samuel Alvarado <Hector.AD@AlliedGlobal.com>
2024-02-23 12:09:38 -0600

    adding the whole test unit for the table-bench

--- src/test/java/com/rdmfinal/TablesReservation_Final/application/service/TableBenchServiceTest.java
+++ src/test/java/com/rdmfinal/TablesReservation_Final/application/service/TableBenchServiceTest.java
@@ -4,13 +4,18 @@ import com.rdmfinal.TablesReservation_Final.application.service.PostGreSQL.Table
 import com.rdmfinal.TablesReservation_Final.domain.dto.TableBenchDTO;
 import com.rdmfinal.TablesReservation_Final.domain.entity.PostGreSQL.TableBench;
 import com.rdmfinal.TablesReservation_Final.domain.repository.PostGreSQL.TableBenchRepository;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class TableBenchServiceTest {
@@ -35,5 +40,34 @@ public class TableBenchServiceTest {
         // Then
         verify(tableBenchRepository).save(any(TableBench.class));
     }
+
+    @Test
+    @SneakyThrows
+    void testTableBenchFindById() {
+        // Given
+        final long id = 1;
+        // Retorna el service
+        TableBenchDTO tableBenchDTO = new TableBenchDTO(
+                1L,
+                2,
+                "Jardin",
+                true
+        );
+        // Retorno del repository
+        TableBench expectedDto = TableBench.builder()
+                .id(1L)
+                .noPersons(2)
+                .ubication("Jardin")
+                .isReserved(true)
+                .build();
+
+        // When
+        when(tableBenchRepository.findById(id)).thenReturn(Optional.of(expectedDto));
+        TableBenchDTO resultDto = tableBenchService.findMesaById(id);
+
+        // Then
+        assertEquals(tableBenchDTO, resultDto);
+    }
+
 
 }
